015697f8b7b3c264911dc73da82551ccb8c535c6
tangxiya-star <tangxiya9906@gmail.com>
2026-04-30 22:26:47 -0700

    add basePath /scanreason for portfolio rewrite

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/scanreason",
+  assetPrefix: "/scanreason",
   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
